3243782a8641f473188ee4bfd2c59b309ef28636
guilherm-hsbe <guilherm3hsb@gmail.com>
2024-06-08 21:32:42 -0300

    initial commit

---
+++ main.js
@@ -0,0 +1,10 @@
+import 'bootstrap/dist/js/bootstrap.js';
+import 'aos/dist/aos.css';
+import AOS from 'aos';
+import './style.css';
+
+AOS.init({
+    offset: 300,
+    duration: 700,
+    once: true,
+});
